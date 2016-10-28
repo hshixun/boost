793e64729e60799363e10023dca3b4738f99356e
Enrico <hshixun@yahoo.com>
2016-10-27 21:05:42 -0400

    add icu add icu build add boost bootstrap with icu

--- index.js
+++ index.js
@@ -7,14 +7,22 @@ var getBoostVersion = require('./lib/getBoostVersion');
 var downloadBoost = require('./lib/downloadBoost');
 var extractBoost = require('./lib/extractBoost');
 var buildBoost = require('./lib/buildBoost');
+var downloadICU = require('./lib/downloadICU');
+var extractICU = require('./lib/extractICU');
+var buildICU = require('./lib/buildICU');
 
 var downloadDir = path.join(__dirname, "download");
 var buildDir = path.join(__dirname, "build");
+var installDir = path.join(__dirname, "third_party");
 
 if(!fs.existsSync(downloadDir)) fs.mkdirSync(downloadDir);
 if(!fs.existsSync(buildDir)) fs.mkdirSync(buildDir);
+if(!fs.existsSync(installDir)) fs.mkdirSync(installDir);
 
-async.waterfall([ 
+async.waterfall([
+  downloadICU,
+  extractICU,
+  buildICU,
   getBoostVersion,
   downloadBoost,
   extractBoost,
